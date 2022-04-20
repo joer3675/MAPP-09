727a66e9d54649ecbfe241048e5e37039658256f
Tanya <tanyaforsell@gmail.com>
2022-04-20 11:12:54 +0200

    Update CalcController.cs
    Någon form av algoritm som dock ännu inte funkar

--- Blackout/Assets/Scripts/CalcController.cs
+++ Blackout/Assets/Scripts/CalcController.cs
@@ -5,11 +5,49 @@ using UnityEngine;
 public class CalcController : MonoBehaviour
 {
     private string inputA;
+    private float inputAlchohol = 0;
+    private double isWoman = 4.7;
+    private double isMan = 3.75;
+    private bool Woman = true;
+    private double ouncesOfAlchohol;
+    private string a;
+    private string b;
 
     public void GetInputWeight(string weight)
     {
         inputA = weight;
         Debug.Log(inputA);
     }
-   
+
+    public void GetBACLevelMan()
+    {
+
+        a = ouncesOfAlchohol * 3.75;
+        double BACMan = a / inputA;
+        Debug.Log(BACMan);
+
+    }
+
+    public void GetBACLevelWoman()
+    {
+
+        b = ouncesOfAlchohol * 4.7;
+        double BACWoman = b / inputA;
+        Debug.Log(BACWoman);
+
+    }
+
+    public void GetBACLevel()
+    {
+        if (Woman == true)
+        {
+            GetBACLevelWoman;
+        }
+
+        else
+        {
+            GetBACLevelMan;
+        }
+    }
+
 }
