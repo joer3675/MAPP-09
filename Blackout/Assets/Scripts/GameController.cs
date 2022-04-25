43e3b4cf54d9a00a0689c9bd3f1fdf66d838854f
Johan Erlesand <johan.erlesand@gmail.com>
2022-04-25 10:40:39 +0200

    Fixat så att det går att hoppa mellan scener i Main
    Råkade jobba lite i Main med att fixa att gå mellan scener

--- Blackout/Assets/Scripts/GameController.cs
+++ Blackout/Assets/Scripts/GameController.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,29 +9,58 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private SwapScene swapScene;
 
+
     [SerializeField] private string loadSceneSaved;
     public Button[] sceneOneButtons;
+    //private string s;
 
     private Button btnPressed;
 
-    // // void Start()
-    // // {
-    // //     foreach (Button btn in sceneOneButtons)
-    // //     {
-    // //         btn.onClick.AddListener(TaskOnClick);
-    // //     }
-
-    // // }
-
-    // // void TaskOnClick()
-    // // {
-
-    // // }
-
-
-    public void ButtonSavedPressed()
+    void Start()
     {
-        swapScene.LoadScene();
+        foreach (Button btn in sceneOneButtons)
+        {
+            btn.onClick.AddListener( () => LoadScene(getScene(btn.name)) );
+        }
+
     }
+    public void LoadScene(string sceneName){
+        if(sceneName!= null)
+        swapScene.LoadScene(sceneName);
+    }
+
+    public string getScene(string buttonName){
+        switch(buttonName)
+        {
+            case "Button_Scene_StartGame":
+                return "StartGame";
+            case "Button_Scene_Timeline": 
+                return "Timeline";
+            case "Button_Scene_PromilleCalc":
+                return "PromilleCalc";
+            case "Button_Save":
+                return "Menu";
+            default:
+                return null;
+        }
+
+    }
+
+    // public void SceneStartGame(){
+    //     swapScene.LoadScene("StartGame");
+    // }
+    // public void SceneTimeline(){
+    //     swapScene.LoadScene("Timeline");
+    // }
+    // public void ScenePromilleCalc(){
+    //     swapScene.LoadScene("PromilleCalc");
+    // }
+    // public void SceneOption(){
+    //     swapScene.LoadScene("StartMenu");
+    // }
+    // public void ButtonSavedPressed()
+    // {
+    //     swapScene.LoadScene("Menu");
+    // }
 
 }
--- Blackout/Assets/Scripts/SwapScene.cs
+++ Blackout/Assets/Scripts/SwapScene.cs
@@ -5,14 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class SwapScene : MonoBehaviour
 {
-    [SerializeField] public string nameOfScene;
-    void awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-    }
-    public void LoadScene()
-    {
-        
+    //[SerializeField] public string nameOfScene;
+    
+    // public GameObject gameObject;
+
+   
+    // void Awake()
+    // {
+    //     //ReadUserInput readUserInput = gameObject.GetCompontent<ReadUserInput>;
+    //     //DontDestroyOnLoad(this.gameObject);
+    // }
+
+
+    // void Start(){
+    //     ReadUserInput readUserInput = gameObject.GetCompontent<ReadUserInput>;
+    //     Debug.Log("Here");
+    //     if(readUserInput.hasInput()){
+    //         LoadScene("Menu");
+    //     }
+    //     Debug.Log(readJsonData.hasInput());
+
+    // }
+
+
+    public void LoadScene(string nameOfScene )
+    {     
         SceneManager.LoadScene(nameOfScene);
 
     }
