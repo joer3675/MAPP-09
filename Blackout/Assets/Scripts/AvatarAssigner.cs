ae877e1afbd5de5403c0fb27c41dd67034178ae8
Tanya <tanyaforsell@gmail.com>
2022-04-26 15:43:36 +0200

    försökt komma igång med avatarerna

---
+++ Blackout/Assets/Scripts/AvatarAssigner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarAssigner : MonoBehaviour
+{
+    // lite osäker kring hur man ska hämta informationen från det andra scriptet,
+    // har nu bara följt en tutorial på yt så vet ej om detta funkar att använda i detta läge...
+
+    ReadUserInput readUserInput;
+    UserData userData;
+    [SerializeField] GameObject playerAvatar;
+    [SerializeField] GameObject manAvatar;
+    [SerializeField] GameObject womanAvatar;
+    [SerializeField] GameObject nonbinaryAvatar;
+    
+
+
+    
+    void Awake()
+    {
+        userData = playerAvatar.GetComponent<UserData>();
+    }
+
+    void Start()
+    {
+        
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
