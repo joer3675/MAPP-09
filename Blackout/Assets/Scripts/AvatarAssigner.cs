ca03913421e91f2899781ca690af5114224bf883
JaVeOls <jacknilsvegaolsson@gmail.com>
2022-04-27 17:26:36 +0200

    Kod
    Jag har gjort lite kod avatar

--- Blackout/Assets/Scripts/AvatarAssigner.cs
+++ Blackout/Assets/Scripts/AvatarAssigner.cs
@@ -1,38 +1,50 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class AvatarAssigner : MonoBehaviour
 {
-    // lite osäker kring hur man ska hämta informationen från det andra scriptet,
-    // har nu bara följt en tutorial på yt så vet ej om detta funkar att använda haha...
-
-    ReadUserInput readUserInput;
-    UserData userData;
-    GameObject playerAvatar;
-    [SerializeField] GameObject manAvatar;
-    [SerializeField] GameObject womanAvatar;
-    [SerializeField] GameObject nonbinaryAvatar;
+    private VariableData _userData = new VariableData();
     
+        [SerializeField] Sprite manAvatar;
+        [SerializeField] Sprite womanAvatar;
+        [SerializeField] Sprite nonbinaryAvatar;
 
-
-    
-    void Awake()
-    {
-        userData = playerAvatar.GetComponent<UserData>();
-    }
-
-    void Start()
-    {
+        void Awake()
         {
-            
+       //_userData = gameObject.GetComponent<VariableData>();
+
         }
+            
+                    
+        void Start()
+        {
+
+            string json = File.ReadAllText(Application.persistentDataPath + "UserData.json");
+            VariableData data = JsonUtility.FromJson<VariableData>(json);
+
+            _userData.sex = data.sex;
+            _userData.age = data.age;
+            _userData.weight = data.weight;
+            _userData.currentIndex = data.currentIndex;
+            _userData.History = data.History;
+
+
+            Debug.Log(_userData.sex + " is the sex.");
+            Debug.Log(_userData.age + " is the age.");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+
+[System.Serializable]
+public class VariableData
+{
+    public string sex;
+    public int age, weight;
+    public int currentIndex = 0;
+    public List<History> History = new List<History>();
+
 }
