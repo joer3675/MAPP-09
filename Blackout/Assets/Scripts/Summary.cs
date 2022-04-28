a54e8679abd7c94919d31f7e60e74dc48a263d1e
SamuelNotSoL <saro2753@student.su.se>
2022-04-28 13:05:22 +0200

    Förfining av summary script
    PrepWork för att färdigställa timeline funktionen.

--- Blackout/Assets/Scripts/Summary.cs
+++ Blackout/Assets/Scripts/Summary.cs
@@ -2,26 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Newtonsoft.Json;
+
+
+public class DrinkCollector
+{
+
+    public List<History> drinkList;
+
+}
 
 
 public class Summary : MonoBehaviour
 {
+    private DrinkCollector drinkCollector = new DrinkCollector();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Text txtMy = GameObject.Find("Panel/Start").GetComponent<Text>();
-        txtMy.text = "Kvällens dricka";
+        getData();
 
-        Text txtMy1 = GameObject.Find("Panel(1)/FirstDrink").GetComponent<Text>();
+
+        Text txtMy = GameObject.Find("Panel1/Text").GetComponent<Text>();
+        txtMy.text = "Your Night Started!";
+
+        Text txtMy1 = GameObject.Find("Panel2/Text").GetComponent<Text>();
         txtMy1.text = "Beer kl 20:30";
 
-        Text txtMy2 = GameObject.Find("Panel(2)/SecondDrink").GetComponent<Text>();
-        txtMy2.text = "Beer kl 20:30";
+        Text txtMy2 = GameObject.Find("Panel3/Text").GetComponent<Text>();
+        txtMy2.text = "Beer kl 20:58";
+
+        Text txtMy3 = GameObject.Find("Panel4/Text").GetComponent<Text>();
+        txtMy3.text = "Beer kl 21:30";
+
+        Text txtMy4 = GameObject.Find("Panel5/Text").GetComponent<Text>();
+        txtMy4.text = "Beer kl 21:45";
+
+        Text txtMy5 = GameObject.Find("Panel6/Text").GetComponent<Text>();
+        txtMy5.text = "Shot kl 21:59";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void getData() {
+
+        GameData myData = DataHandler.LoadGameData();
+        drinkCollector.drinkList = myData.History;
+
+        foreach (History h in drinkCollector.drinkList) {
+            Debug.Log(h._Drinks);
+        }
     }
+    
 }
