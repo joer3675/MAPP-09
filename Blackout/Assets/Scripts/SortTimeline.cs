6988f31105d15ca98a4d1175c3fbabfc357659f6
Tanya <tanyaforsell@gmail.com>
2022-04-29 13:20:44 +0200

    sorteringfilter av något slag

--- Blackout/Assets/Scripts/SortTimeline.cs
+++ Blackout/Assets/Scripts/SortTimeline.cs
@@ -36,21 +36,63 @@ public class SortTimeline : MonoBehaviour
 
     void DropdownItemSelected(Dropdown dropdown)
     {
-        Debug.Log("i'm here");
+        //Debug.Log("i'm here");
+
+
 
         GameData myData = DataHandler.LoadGameData();
 
-
+        List<History> myHistory = myData.History;
         
-        myDrinks = history._Drinks;
-        Debug.Log(myDrinks.Count);
+        
+        
 
-        Dictionary<int, string> myBeer;
+        //myDrinks = myData.History[0]._Drinks;
+
+        //Debug.Log(myDrinks.Count);
+
+        foreach (History history in myHistory)
+        {
+            
+
+            foreach (Drinks drinks in history._Drinks)
+            {
+                myDrinks.Add(drinks);
+
+                /* foreach (var i in drinks.beer)
+                {
+                    Debug.Log(i+" beer");
+                }
+                foreach (var i in drinks.wine)
+                {
+                    Debug.Log(i + " wine");
+                }
+                foreach (var i in drinks.shot)
+                {
+                    Debug.Log(i + " shot");
+                } */
+
+                //Debug.Log(drinks.beer[2]);
+                /*Debug.Log(drinks.wine[0]);
+                Debug.Log(drinks.shot[0]); */
+            }
+        }
+
+        /* Dictionary<int, string> myBeer;
+
 
         foreach (Drinks drinks in myDrinks)
         {
             Debug.Log(drinks.beer);
             Debug.Log("here");
         }
+        */
+
+        foreach (Drinks drinks in myDrinks)
+        {
+            
+        }
+
     }
+
 }
