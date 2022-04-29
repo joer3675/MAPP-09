e342359538036062be5c8c0b620e705e36d28718
Tanya <tanyaforsell@gmail.com>
2022-04-29 12:14:07 +0200

    hej

--- Blackout/Assets/Scripts/SortTimeline.cs
+++ Blackout/Assets/Scripts/SortTimeline.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 
 public class SortTimeline : MonoBehaviour
 {
-    
+    private History history = new History();
+    private List<Drinks> myDrinks = new List<Drinks>();
 
     // Start is called before the first frame update
     void Start()
     {
+        
+
         var dropdown = gameObject.GetComponent<Dropdown>();
 
         dropdown.options.Clear();
 
         List<string> items = new List<string>();
-        items.Add("Promille");
-        items.Add("Distance");
+        items.Add("");
+        items.Add("");
 
 
         foreach (var item in items)
@@ -33,6 +36,21 @@ public class SortTimeline : MonoBehaviour
 
     void DropdownItemSelected(Dropdown dropdown)
     {
+        Debug.Log("i'm here");
+
+        GameData myData = DataHandler.LoadGameData();
+
+
         
+        myDrinks = history._Drinks;
+        Debug.Log(myDrinks.Count);
+
+        Dictionary<int, string> myBeer;
+
+        foreach (Drinks drinks in myDrinks)
+        {
+            Debug.Log(drinks.beer);
+            Debug.Log("here");
+        }
     }
 }
