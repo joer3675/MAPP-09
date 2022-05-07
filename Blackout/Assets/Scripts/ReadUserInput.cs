6f71081fc28b167f50045fdc2681b01bf1e454e3
Johan Erlesand <johan.erlesand@gmail.com>
2022-05-07 12:01:43 +0200

    Johan commit

--- Blackout/Assets/Scripts/ReadUserInput.cs
+++ Blackout/Assets/Scripts/ReadUserInput.cs
@@ -1,18 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 public class ReadUserInput : MonoBehaviour
 {
     private UserData _userData = new UserData();
-    //[SerializeField] private SwapScene swapScene;
-    [SerializeField] private Dropdown dropDownSex;
+    [SerializeField] private Sprite boy;
+    [SerializeField] private Sprite boyPressed;
+    [SerializeField] private Sprite girl;
+    [SerializeField] private Sprite girlPressed;
+    [SerializeField] private Button[] buttonSex;
+
     [SerializeField] private InputField inputFieldAge, inputFieldWeight;
 
 
+    void Start()
+    {
+        foreach (Button button in buttonSex)
+        {
+            button.onClick.AddListener(() => SetGender(button));
+        }
+    }
     public void SaveToJson()
     {
         int.TryParse(inputFieldAge.text, out _userData.age);
         int.TryParse(inputFieldWeight.text, out _userData.weight);
-        _userData.sex = dropDownSex.options[dropDownSex.value].text;
+        //_userData.sex = dropDownSex.options[dropDownSex.value].text;
 
         if (hasInput())
         {
@@ -22,6 +34,32 @@ public class ReadUserInput : MonoBehaviour
         }
     }
     public bool hasInput() { return _userData.age > 0 && _userData.weight > 0 ? true : false; }
+
+    private void SetGender(Button btn)
+    {
+        foreach (Button b in buttonSex)
+        {
+            if (b.name == "Button_Female")
+            {
+                b.GetComponent<Image>().sprite = girl;
+            }
+            else
+            {
+
+                b.GetComponent<Image>().sprite = boy;
+            }
+        }
+        if (btn.name == "Button_Male")
+        {
+            btn.GetComponent<Image>().sprite = boyPressed;
+            _userData.sex = "Male";
+        }
+        else
+        {
+            btn.GetComponent<Image>().sprite = girlPressed;
+            _userData.sex = "Female";
+        }
+    }
 
 }
 
