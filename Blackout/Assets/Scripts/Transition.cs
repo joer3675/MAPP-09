7c46f785094276f9470d75d39c260a75534449b9
Ellen <ellen.storgard@icloud.com>
2022-05-10 18:38:40 +0200

    swipe animation

---
+++ Blackout/Assets/Scripts/Transition.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public Animator animator;
+    public float transitionDelayTime = 0.5f;
+
+    void Awake()
+    {
+        animator = GameObject.Find("Transition").GetComponent<Animator>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            LoadLevel();
+        }
+        
+    }
+
+    public void LoadLevel()
+    {
+        StartCoroutine(DelayLoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator DelayLoadLevel(int index)
+    {
+        animator.SetTrigger("TriggerTransition");
+        yield return new WaitForSeconds(transitionDelayTime);
+        SceneManager.LoadScene(index);
+    }
+}
