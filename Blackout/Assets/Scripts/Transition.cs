55b657ccd3c86539de155651886625bfcb46a930
Ellen <ellen.storgard@icloud.com>
2022-05-11 15:38:17 +0200

    ny layout på kalk

--- Blackout/Assets/Scripts/Transition.cs
+++ Blackout/Assets/Scripts/Transition.cs
@@ -19,7 +19,7 @@ public class Transition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.GetKey(KeyCode.Space))
         {
             LoadLevel();
         }
