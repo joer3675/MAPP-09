2bfa2847c190847fcf5fae538f712d88f35e8580
JaVeOls <jacknilsvegaolsson@gmail.com>
2022-05-18 12:22:50 +0200

    kod

--- Blackout/Assets/Scripts/AvatarAssigner.cs
+++ Blackout/Assets/Scripts/AvatarAssigner.cs
@@ -6,35 +6,44 @@ using System.IO;
 public class AvatarAssigner : MonoBehaviour
 {
     private VariableData _userData = new VariableData();
-    
-        [SerializeField] Sprite manAvatar;
-        [SerializeField] Sprite womanAvatar;
-        [SerializeField] Sprite nonbinaryAvatar;
 
-        void Awake()
-        {
-       //_userData = gameObject.GetComponent<VariableData>();
+    [SerializeField] private SpriteRenderer manAvatar;
+    [SerializeField] private SpriteRenderer womanAvatar;
+    [SerializeField] private SpriteRenderer naryAvatar;
 
-        }
-            
-                    
-        void Start()
-        {
-
-            string json = File.ReadAllText(Application.persistentDataPath + "UserData.json");
-            VariableData data = JsonUtility.FromJson<VariableData>(json);
-
-            _userData.sex = data.sex;
-            _userData.age = data.age;
-            _userData.weight = data.weight;
-            _userData.currentIndex = data.currentIndex;
-            _userData.History = data.History;
+    CalcController calcController;
 
 
-            Debug.Log(_userData.sex + " is the sex.");
-            Debug.Log(_userData.age + " is the age.");
+    void Awake()
+    {
+        //_userData = gameObject.GetComponent<VariableData>();
+
+        calcController = GameObject.Find("Button_Calculate").GetComponent<CalcController>;()
 
     }
+
+
+    void Start()
+    {
+
+        string json = File.ReadAllText(Application.persistentDataPath + "UserData.json");
+        VariableData data = JsonUtility.FromJson<VariableData>(json);
+
+        _userData.sex = data.sex;
+        _userData.age = data.age;
+        _userData.weight = data.weight;
+        _userData.currentIndex = data.currentIndex;
+        _userData.History = data.History;
+
+
+        Debug.Log(_userData.sex + " is the sex.");
+        Debug.Log(_userData.age + " is the age.");
+        Debug.Log(_userData.weight + " is the weight.");
+
+
+    }
+
+
 
 }
 
@@ -48,3 +57,7 @@ public class VariableData
     public List<History> History = new List<History>();
 
 }
+
+// N?sta uppgift h?mta promille fr?n Johans promille
+// Fixa s? att avatar ?ndras beroende k?n
+// Sedan promillehalt (hur full?)
