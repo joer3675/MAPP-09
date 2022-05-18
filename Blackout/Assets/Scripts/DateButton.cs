4ae2b93d4f0e10b1680c633553b0b1bf637959c9
SamuelNotSoL <saro2753@student.su.se>
2022-05-18 11:29:47 +0200

    Skapat nya scener
    Skapat nya scener och skrivit pseudokod i tre scripts

---
+++ Blackout/Assets/Scripts/DateButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateButton : MonoBehaviour
+{
+    // En knapp som när man trycker på en vald panel så öppnas en större panel med mer info på.
+    //Leta up paneler och skriv ut  kvälls grund info på dem t.ex (__:__:__Date, _,_ promille)
+    // Find.Button.Namn.text. print från listan på json. 
+    // if(button pressed){       ........Poppa upp en större panel som har all info på sig ifrån kvällen.
+    // spawn panel(med resterande av kvällens info)
+    // add scrollbar på panel som används om de varit en längre kväll. 
+    //Skrivut all info ifrån kvällen;
+    // }
+
+}
---
+++ Blackout/Assets/Scripts/NightDataD.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightDataD : MonoBehaviour
+{
+    //skriva ut dem kvällar som man haft i ordning, med nyaste kvällen först.
+    //Vill skriva ut varje enskilld kväll på en panel som ligger i en scrollbar.
+    //Get info från json
+}
