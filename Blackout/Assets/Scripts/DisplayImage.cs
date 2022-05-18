dd3474c61a038a381b235e04a2b7b6deee1e8fdc
Adam1416k <adamastrom02@gmail.com>
2022-05-18 14:06:16 +0200

    Display image script

---
+++ Blackout/Assets/Scripts/DisplayImage.cs
@@ -0,0 +1,30 @@
+ 
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+ 
+public class DisplayImage : MonoBehaviour
+{
+    Texture2D thisTexture;
+    byte[] bytes;
+    string fileName;
+    public GameObject[] ImageHolder = new GameObject[1];
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        var imagesToLoad = Directory.GetFiles(Application.persistentDataPath + "/screenshots", "*.png"); //insert locatin of file storage
+        for (int i = 0; i < imagesToLoad.Length; i++)
+        {
+            thisTexture = new Texture2D(100, 100);
+            fileName = imagesToLoad[i];
+            bytes = File.ReadAllBytes(fileName);
+            thisTexture.LoadImage(bytes);
+            thisTexture.name = fileName;
+            ImageHolder[i].GetComponent<RawImage>().texture = thisTexture;
+        }
+    }
+}
