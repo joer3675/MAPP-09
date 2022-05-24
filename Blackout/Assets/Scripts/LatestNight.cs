110327ecb11378f210da381328b2d028501abdd9
Tanya <tanyaforsell@gmail.com>
2022-05-24 12:08:02 +0200

    fix

--- Blackout/Assets/Scripts/LatestNight.cs
+++ Blackout/Assets/Scripts/LatestNight.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LatestNight : MonoBehaviour
 {
+    private GameData gameData;
+    private History lastHistory;
+    [SerializeField] private Text historyText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameData = DataHandler.LoadGameData();
+        List<History> historyList = gameData.History;
+        //hämtar sista objektet i listan
+        lastHistory = historyList[historyList.Count - 1];
+        Debug.Log(lastHistory.MaxPromille);
+
+        string tempText = "";
+
+        foreach (Drinks drink in lastHistory._Drinks)
+        {
+
+            
+
+
+
+            tempText += lastHistory.dateCreated + " and you had a max per mille at " + lastHistory.MaxPromille + " \n\n";
+            foreach (KeyValuePair<string, string> kvp in drink.drinks)
+            {
+                tempText += kvp.Key + "  " + kvp.Value + " \n\n";
+
+            }
+
+        }
+
+        historyText.text = tempText;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
 }
