406af19bbce2f43792c0b0b27d0749997da6bc1c
SamuelNotSoL <saro2753@student.su.se>
2022-05-25 10:49:00 +0200

    Löst bug med dubble scripts

--- Blackout/Assets/Scripts/DateSorter 2.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DateSorter : MonoBehaviour
-{
-    // Vill ändra den kronologiska ordning för de paneler som NightDataD har skapat.
-    // Ta den listan som vi har sorterat allt in i vänd på den. 
-}
