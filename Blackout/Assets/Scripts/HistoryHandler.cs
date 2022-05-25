70ef8fad75656c4213425f08de8a484f191363d4
Johan Erlesand <johan.erlesand@gmail.com>
2022-05-25 08:29:20 +0200

    Tog bort oanvända scripts

--- Blackout/Assets/Scripts/HistoryHandler.cs
+++ Blackout/Assets/Scripts/HistoryHandler.cs
@@ -1,62 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using System;
 
-public class HistoryHandler : MonoBehaviour
-{
+// public class HistoryHandler : MonoBehaviour
+// {
 
-    private GameData gameData;
-    public Text TextBox;
-    // Start is called before the first frame update
-    void Start()
-    {
-        try
-        {
-            gameData = DataHandler.LoadGameData();
-
-
-        }
-        catch (Exception e)
-        {
-            Debug.Log("Could not Load data" + e);
-            throw;
-        }
-
-        var dropdown = gameObject.GetComponent<Dropdown>();
-        List<string> items = new List<string>();
-        List<History> list = new List<History>();
-        dropdown.options.Clear();
-        foreach (History h in list)
-        {
-            items.Add(h.dateCreated);
-        }
-
-        items.Add("2022-04-22");
-        items.Add("2022-04-15");
-        items.Add("2022-04-09");
-
-        foreach (var item in items)
-        {
-            dropdown.options.Add(new Dropdown.OptionData() { text = item });
-        }
-
-        DropdownItemSelected(dropdown);
+//     private GameData gameData;
+//     public Text TextBox;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         try
+//         {
+//             gameData = DataHandler.LoadGameData();
 
 
-        dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
+//         }
+//         catch (Exception e)
+//         {
+//             Debug.Log("Could not Load data" + e);
+//             throw;
+//         }
 
-    }
+//         var dropdown = gameObject.GetComponent<Dropdown>();
+//         List<string> items = new List<string>();
+//         List<History> list = new List<History>();
+//         dropdown.options.Clear();
+//         foreach (History h in list)
+//         {
+//             items.Add(h.dateCreated);
+//         }
+
+//         items.Add("2022-04-22");
+//         items.Add("2022-04-15");
+//         items.Add("2022-04-09");
+
+//         foreach (var item in items)
+//         {
+//             dropdown.options.Add(new Dropdown.OptionData() { text = item });
+//         }
+
+//         DropdownItemSelected(dropdown);
 
 
-    void DropdownItemSelected(Dropdown dropdown)
-    {
+//         dropdown.onValueChanged.AddListener(delegate { DropdownItemSelected(dropdown); });
 
-        Debug.Log("date");
-        int index = dropdown.value;
-        TextBox.text = dropdown.options[index].text;
-    }
+//     }
 
 
-}
+//     void DropdownItemSelected(Dropdown dropdown)
+//     {
+
+//         Debug.Log("date");
+//         int index = dropdown.value;
+//         TextBox.text = dropdown.options[index].text;
+//     }
+
+
+// }
