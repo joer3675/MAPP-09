22e74c60fa85913673a105baebccfcc9614c80cf
Johan Erlesand <johan.erlesand@gmail.com>
2022-05-28 16:38:39 +0200

    Fixat så att det fungerar i olika upplösningar nu

--- Blackout/Assets/Scripts/HistortTextScript.cs
+++ Blackout/Assets/Scripts/HistortTextScript.cs
@@ -20,14 +20,21 @@ public class HistortTextScript : MonoBehaviour
     private void displayOnInformationPanel()
     {
         String tempText = "";
-        foreach (Drinks drink in myHistoryObject._Drinks)
+        if (myHistoryObject != null)
         {
-            tempText += myHistoryObject.dateCreated + " and you had a max per mille at " + myHistoryObject.MaxPromille + " \n\n";
-            foreach (KeyValuePair<string, string> kvp in drink.drinks)
+            foreach (Drinks drink in myHistoryObject._Drinks)
             {
-                tempText += kvp.Key + "  " + kvp.Value + " \n\n";
+                tempText += myHistoryObject.dateCreated + " and you had a max per mille at " + myHistoryObject.MaxPromille + " \n\n";
+                foreach (KeyValuePair<string, string> kvp in drink.drinks)
+                {
+                    tempText += kvp.Key + "  " + kvp.Value + " \n\n";
 
+                }
             }
+        }
+        else
+        {
+            tempText = "No History";
         }
         textOnScreen.text = tempText;
     }
