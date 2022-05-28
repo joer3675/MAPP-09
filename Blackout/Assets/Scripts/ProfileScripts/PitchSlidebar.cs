35edcfccd271f9cdeb8918d32665c2797a867e29
Johan Erlesand <johan.erlesand@gmail.com>
2022-05-28 15:34:00 +0200

    SlutFinish

--- Blackout/Assets/Scripts/ProfileScripts/PitchSlidebar.cs
+++ Blackout/Assets/Scripts/ProfileScripts/PitchSlidebar.cs
@@ -1,13 +1,10 @@
 using UnityEngine;
 using UnityEngine.Audio;
-using System.Collections;
 
 public class PitchSlidebar : MonoBehaviour
 {
     public AudioMixer mixer;
     public AudioSource audioSource;
-    private float duration = 0.5f;
-    private float targetVolume = 0;
 
     public void SetPitchLevel(float sliderValue)
     {
@@ -21,18 +18,6 @@ public class PitchSlidebar : MonoBehaviour
 
     public void stopAudio()
     {
-        StartCoroutine(stopDelay());
         audioSource.Stop();
-    }
-
-    IEnumerator stopDelay()
-    {
-        float currentTime = 0;
-        float start = audioSource.volume;
-        while (currentTime < duration)
-        {
-            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
-        }
-        yield return null;
     }
 }
